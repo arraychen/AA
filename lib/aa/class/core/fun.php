0edde6791577f10aa1110174da22b36a1aad317f
hui <hui@com.com>
2016-12-02 18:11:06 +0800

    修改函数名

--- lib/aa/class/core/fun.php
+++ lib/aa/class/core/fun.php
@@ -1,14 +1,14 @@
 <?php
 
 class bFun {
-	public static function pr($var,$type='html') {
+	public static function printR($var,$type='html') {
 		if ($type=='html') {
 			echo '<pre>',htmlspecialchars(print_r($var,1)),'</pre>';
 		} elseif ($type=='js') {
 			echo $var;
 		}
 	}
-	public static function vd($var,$type='html') {
+	public static function varDump($var,$type='html') {
 		var_dump($var);
 	}
 }
